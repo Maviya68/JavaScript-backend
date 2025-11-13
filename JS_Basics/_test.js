457b0c449620154a193f04f199158dcc70361d78
Maviya68 <mohdmavia68@gmail.com>
2025-11-13 07:48:11 +0000

    math.random

---
+++ JS_Basics/_test.js
@@ -0,0 +1,20 @@
+export function add(arg1,arg2){
+    return arg1 + arg2;
+}
+
+// console.log(add(2,3+2+"3"));
+// console.log(null >= 0);
+// console.log(null == 0);
+// console.log(null > 0);
+// console.log(null < 0);
+// const String = "Pomodorp"
+// const newString = String.substring(0,8)
+// console.log(newString);
+// const slicedString = String.slice(-5,7) // '-' index starts from the right(my opinion)
+// console.log(slicedString); 
+const my = "Every green bus drives fast.";
+//Getting 't' with different methods 
+// console.log(my.charAt(my.length-2)); 
+// console.log(my.substring(my.length-2,my.length-1));
+// console.log(my.slice(-2,-1))
+// console.log(my.at(-2))
